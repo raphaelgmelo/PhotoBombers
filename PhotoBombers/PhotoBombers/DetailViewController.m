00eb9dd9768498331c09439da8763a21dc14e47c
Raphael Melo <raphaelgmelo@gmail.com>
2015-10-18 21:19:30 -0200

    transparent background

--- PhotoBombers/PhotoBombers/DetailViewController.m
+++ PhotoBombers/PhotoBombers/DetailViewController.m
@@ -20,7 +20,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.view.backgroundColor = [UIColor whiteColor];
+    self.view.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.95];
 
     self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, 320)];
     [self.view addSubview:self.imageView];
